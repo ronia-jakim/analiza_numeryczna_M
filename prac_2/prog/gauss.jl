ec8dc5bb7b405cc1047717fd2d4903a318acdd63
Łukasz Magnuszewski <lukasz.magnuszewski@gmail.com>
2023-01-04 09:07:37 +0100

    modulurazyja gaussa

--- prac_2/prog/gauss.jl
+++ prac_2/prog/gauss.jl
@@ -1,27 +1,30 @@
 
+module gauss
 
-function gauss(matrix, n)
-  for i in 1:(n-1)
-    for j in (i+1):n
-      ratio = matrix[j, i] / matrix[i, i]
-      for k in 1:(n+1)
-        matrix[j, k] = matrix[j, k] - ratio * matrix[i, k]
+  function gauss(matrix, n)
+    for i in 1:(n-1)
+      for j in (i+1):n
+        ratio = matrix[j, i] / matrix[i, i]
+        for k in 1:(n+1)
+          matrix[j, k] = matrix[j, k] - ratio * matrix[i, k]
+        end
       end
     end
+    return matrix
   end
-  return matrix
-end
 
-function back_substitution(matrix, n)
-  solutions = fill(0.0, n)
-  solutions[n] = matrix[n, n+1] / matrix[n, n]
-  for i in (n-1):1
-    solutions[i] = matrix[i, n+1]
-    for j in (i+1):n
-      solutions[i] = solutions[i] - matrix[i, j] * solutions[j]
+  function back_substitution(matrix, n)
+    solutions = fill(0.0, n)
+    solutions[n] = matrix[n, n+1] / matrix[n, n]
+    for i in (n-1):1
+      solutions[i] = matrix[i, n+1]
+      for j in (i+1):n
+        solutions[i] = solutions[i] - matrix[i, j] * solutions[j]
+      end
+      solutions[i] = solutions[i] / matrix[i, i]
     end
-    solutions[i] = solutions[i] / matrix[i, i]
+    return solutions
   end
-  return solutions
+
 end
 
