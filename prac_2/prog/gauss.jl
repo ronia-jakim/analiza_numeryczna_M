3e5fa6f36074cee951e7e708caa7dd37515d11c2
Łukasz Magnuszewski <lukasz.magnuszewski@gmail.com>
2023-01-04 09:04:21 +0100

    gaussik

---
+++ prac_2/prog/gauss.jl
@@ -0,0 +1,27 @@
+
+
+function gauss(matrix, n)
+  for i in 1:(n-1)
+    for j in (i+1):n
+      ratio = matrix[j, i] / matrix[i, i]
+      for k in 1:(n+1)
+        matrix[j, k] = matrix[j, k] - ratio * matrix[i, k]
+      end
+    end
+  end
+  return matrix
+end
+
+function back_substitution(matrix, n)
+  solutions = fill(0.0, n)
+  solutions[n] = matrix[n, n+1] / matrix[n, n]
+  for i in (n-1):1
+    solutions[i] = matrix[i, n+1]
+    for j in (i+1):n
+      solutions[i] = solutions[i] - matrix[i, j] * solutions[j]
+    end
+    solutions[i] = solutions[i] / matrix[i, i]
+  end
+  return solutions
+end
+
